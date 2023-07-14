840574ac40862768bba03992ba3d7ab1b7ff9281
rana <ranasameh290@gmail.com>
2023-07-13 18:15:10 -0700

    putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Using putchar function to print a statement
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
