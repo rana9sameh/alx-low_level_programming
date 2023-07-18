3406d40f6184b4e56bbed9a239336cb20a8bc466
rana <ranasameh290@gmail.com>
2023-07-18 05:02:17 -0700

    lower except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program prints lowercase except e and q
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char lower = 97;
+
+	while (lower < 123)
+	{
+		if (lower != 101 || lower != 113)
+			break;
+		putchar(lower);
+		lower++;
+	}
+	putchar('\n');
+	return (0);
+}
