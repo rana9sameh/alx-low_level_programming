a73c6ce90ae6151a2fa5997518dbe769247f8551
rana <ranasameh290@gmail.com>
2023-07-18 04:24:32 -0700

    lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A program prints the lowercase alphabets
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	for (char alpha = 97; alpha < 123; alpha++)
+		putchar(alpha);
+	return (0);
+}
+
