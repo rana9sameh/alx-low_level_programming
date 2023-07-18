caa6d1291b3913ebd63d1037d561ce22672474c0
rana <ranasameh290@gmail.com>
2023-07-18 05:49:22 -0700

    putchar 0 to 9

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,11 +11,10 @@
 int main(void)
 {
 	char num [10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	int n = 0;
 
-	while (true)
+	while (num[n] <= 9)
 	{
-		int n;
-
 		putchar(num[n]);
 		n++;
 	}
