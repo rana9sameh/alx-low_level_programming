372eaaace7306ef7f72a948de33e28f9d485f08e
rana <ranasameh290@gmail.com>
2023-07-18 06:18:48 -0700

    from 1 to 9 separated with ,

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program prints from 0 to 9 separated with ,
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar('0' + n);
+		putchar(", ");
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
