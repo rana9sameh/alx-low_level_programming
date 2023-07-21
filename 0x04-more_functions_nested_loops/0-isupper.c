5204351ec863fd2b2340572e80fe115bfb51920d
rana <ranasameh290@gmail.com>
2023-07-21 15:11:09 -0700

    digit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * _isupper - A function that checks if c is upper
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,5 +5,6 @@
 
 int _putchar(char ch);
 int _isupper(int c);
+int _isdigit(int c);
 
 #endif
