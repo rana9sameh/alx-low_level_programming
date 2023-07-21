7719928b6a0d6b8f7636dc6db842347d4a9bd540
rana <ranasameh290@gmail.com>
2023-07-21 16:02:22 -0700

    diagonal with '\'

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - a function that draws a diagonal line on the termina
+ *
+ * @n: is the nof times that \ char should be printed
+*/
+
+void print_diagonal(int n)
+{
+	int postn, space;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (postn = 1; postn <= n; postn++)
+		{
+			for (sspace = 1; space <= postn; space++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
