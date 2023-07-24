6b12a046bf2c16ede4a057a7fed7f65a04cbbc5d
rana <ranasameh290@gmail.com>
2023-07-24 16:15:03 -0700

    fix errors

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_rev - a function that prints a string, in reverse, followed by a new line
+ * print_rev - a function that prints a string,
+ * in reverse, followed by a new line
  *
  * @s: char parameter
 */
