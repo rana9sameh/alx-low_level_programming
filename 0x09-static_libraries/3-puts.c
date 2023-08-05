492c5515419e83af99b60663024d1b150c2d319a
rana <ranasameh290@gmail.com>
2023-08-04 17:28:34 -0700

    s

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,17 +1,18 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _puts - a function that prints a string, followed by a new line, to stdout
  *
- * @str: char parameter
-*/
-
+ * @str: a char pointer
+ */
 void _puts(char *str)
 {
-	while (*str != '\0')
+	int i = 0;
+
+	while (str[i])
 	{
-		_putchar(*str + 0);
-		str++;
+		_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,15 +1,14 @@
 #include <unistd.h>
-#include "main.h"
 
 /**
- * _putchar - write the character ch to stdout
- * @ch: The character to print
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: on success 1
- *         on error  -1 is returned
-*/
-
-int _putchar(char ch)
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-        return (write(1, &ch, 1));
+	return (write(1, &c, 1));
 }
+
